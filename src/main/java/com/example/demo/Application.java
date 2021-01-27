a1073bc4e95f0fcdca0517edbf2eee4c5371bc4d
praveensome <reddyp028@gmail.com>
2021-01-27 13:16:38 +0530

    changes apply

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -8,6 +8,11 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		public void doProcess(){
+		
+		int a =10;
+		int b =20;
+		}
 	}
 
 }
